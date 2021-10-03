363036cc2221c5b254257315495c20ad98db9c06
trakker01 <gabormaxim02@gmail.com>
2021-10-03 18:51:09 +0300

    0.1

--- Proiect-1/src/Apartament.java
+++ Proiect-1/src/Apartament.java
@@ -1,7 +1,64 @@
+import java.util.*;
+import java.lang.String;
+import java.util.Scanner;
+
 public class Apartament {
-    private int Put_price;
-    private String Put_address;
-    private int Put_room;
+
+    Scanner sc = new Scanner(System.in);
+
+    private double Put_price;
+    private String Put_address = "";
+    private String Tip = "Apartament";
+    private double Put_room;
     private double Put_meters;
     private double Put_number;
+
+    public double setPrice(double price)
+    {
+        this.Put_price = sc.nextDouble();
+        return this.Put_price;
+    }
+
+    public double getPut_price() {
+        return this.Put_price;
+    }
+
+    public String setPut_address(String address){
+        this.Put_address = address;
+        return this.Put_address;
+    }
+    public String getPut_address(){
+        return this.Put_address;
+    }
+
+    public double setPut_room(double room)
+    {
+        this.Put_room = sc.nextDouble();
+        return this.Put_room;
+    }
+    public double getPut_room() {
+        return this.Put_room;
+    }
+
+    public double setPut_meters(double meters)
+    {
+        this.Put_meters = sc.nextDouble();
+        return this.Put_meters;
+    }
+    public double getPut_meters() {
+        return this.Put_meters;
+    }
+
+    public double setPut_number(double number)
+    {
+        this.Put_number = sc.nextDouble();
+        return this.Put_number;
+    }
+    public double getPut_number() {
+        return this.Put_number;
+    }
+
+    public String toString(){
+        return Tip + this.Put_price + this. Put_address + this.Put_meters + this.Put_room + this.Put_number;
+    }
 }
--- Proiect-1/src/Site.java
+++ Proiect-1/src/Site.java
@@ -1,10 +1,23 @@
 import java.lang.String;
 import java.util.Scanner;
+import java.util.Vector;
+import java.io.File;
 
-public class Site {
-    public static void main(String[] args) {
+public class Site
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
+        int price, room, meter, number;
+        int n = 5;
+        String address;
+
+        File Lista_apartamente = new File("Apartamente.txt");
+
         Apartament apart = new Apartament();
+
+        Vector<String> v = new Vector<String>(n);
+
         char access_menu = 'y';
         while (access_menu == 'y' || access_menu == 'Y')
         {   System.out.println("Bine ati venit pe site-ul de chirii. Tastati numarul respctiv pentru meniu-ul care doriti sa accesati ");
@@ -35,6 +48,26 @@ public class Site {
             System.out.println("Daca doriti sa va intoarceti la meniul prinicpal apasati y daca nu apasati orice altat tasta");
             access_menu = sc.next().charAt(0);
             System.out.println("\n\n");
-     }
+        }
+
+
+        // Appending new elements at
+        // the end of the vector
+        for (int i = 1; i <= n; i++)
+            v.add(apart.toString());
+
+        // Printing elements
+        System.out.println(v);
+
+        // Remove element at index 3
+        v.remove(3);
+
+        // Displaying the vector
+        // after deletion
+        System.out.println(v);
+
+        // Printing elements one by one
+        for (int i = 0; i < v.size(); i++)
+            System.out.print(v.get(i) + " ");
     }
 }
